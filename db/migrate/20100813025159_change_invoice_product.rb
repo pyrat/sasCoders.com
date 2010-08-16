678e7055a59e2d5b2d933e4e01a559df458c756e
Stephen Philp <swelltrain@gmail.com>
2010-08-16 14:23:12 -0700

    hope this works

--- db/migrate/20100813025159_change_invoice_product.rb
+++ db/migrate/20100813025159_change_invoice_product.rb
@@ -1,6 +1,7 @@
 class ChangeInvoiceProduct < ActiveRecord::Migration
   def self.up
-    change_column :Invoices, :product, :integer
+    remove_column :Invoices, :product
+    add_column :product, :integer
   end
 
   def self.down
