d79a02d6f87f5825bb0acfa4c44982c59fc1fd5a
Stephen Philp <swelltrain@gmail.com>
2010-10-19 11:46:13 -0700

    Hopefully Fixed Job Seeker Migration

--- db/migrate/20100826044415_job_seekers.rb
+++ db/migrate/20100826044415_job_seekers.rb
@@ -2,7 +2,7 @@ class JobSeekers < ActiveRecord::Migration
   def self.up
     create_table :job_seekers do |t|
       t.string :email
-      t.binary :cv, :limit => 1.megabyte
+      t.binary :cv  # this did not work on heroku, :limit => 1.megabyte
     end
   end
 
